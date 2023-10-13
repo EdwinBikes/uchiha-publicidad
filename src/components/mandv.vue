d564dadbf467913e8776065b8c61383a3536dee6
Edwinbikes <67319437+EdwinBikes@users.noreply.github.com>
2023-10-13 11:34:59 -0500

    Update mandv.vue prueba

--- src/components/mandv.vue
+++ src/components/mandv.vue
@@ -1,82 +1,91 @@
 <template>
-        <div class="container__mision">
-                    <div class="container__mision--text">  
-                        <h2>Misión</h2>
-                        <p>
-                        Nuestra misión es simple pero poderosa: colaborarte para alcanzar tus objetivos digitales con éxito. Creemos que cada cliente es único y, por lo tanto, cada solución que ofrecemos es personalizada y adaptada a tus necesidades específicas. Nos esforzamos por ser tu socio de confianza en el espacio digital, ofreciendo servicios que te permitan destacar y crecer en línea..
-                    </p>
-                    </div>
-                    <div class="container__mision--img">
-                        <img src="https://images.pexels.com/photos/6483623/pexels-photo-6483623.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="">
-                    </div>     
-        </div>
+  <div class="container__mision">
+    <div class="container__mision--text">
+      <h2>Misión</h2>
+      <p>
+        Nuestra misión es simple pero poderosa: colaborarte para alcanzar tus
+        objetivos digitales con éxito. Creemos que cada cliente es único y, por
+        lo tanto, cada solución que ofrecemos es personalizada y adaptada a tus
+        necesidades específicas. Nos esforzamos por ser tu socio de confianza en
+        el espacio digital, ofreciendo servicios que te permitan destacar y
+        crecer en línea.
+      </p>
+    </div>
+    <div class="container__mision--img">
+      <img
+        src="https://images.pexels.com/photos/6483623/pexels-photo-6483623.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+        alt=""
+      />
+    </div>
+  </div>
 </template>
 <style scoped>
+.container__mision {
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  height: 500px;
+}
 
-.container__mision{
-    display: flex;
-    justify-content: space-around;
-    align-items: center;
-    height: 560px;
+.container__mision--text,
+.container__mision--img {
+  height: 500px;
+  width: 45vw;
+  background: linear-gradient(
+    to bottom,
+    #f600fe,
+    #c900fe,
+    #a136ff,
+    #5142f0,
+    #0033d9
+  );
+  box-shadow: 4px 4px 2px 2px rgba(0, 0, 0, 0.7);
+  border-radius: 6px;
+  display: flex;
+  justify-content: center;
 }
-.container__mision--text{
-    height: 500px;
-    width: 45vw;
-    background: linear-gradient(to bottom, #f600fe, #c900fe, #a136ff, #5142f0, #0033d9);
-    box-shadow: 4px 4px 2px 2px rgba(0,0,0,0.7);
-    color:white;
-    border-radius: 6px;
-    display: flex;
-    justify-content: center;
-    align-items:stretch;
-    flex-wrap: wrap;
+
+.container__mision--text {
+  color: white;
+  flex-direction: column;
 }
-.container__mision--text h2{
-    width: 100%;
-    text-align: center;
-    font-size: 40px;
+
+.container__mision--img {
+  display: flex;
+  align-items: center;
 }
-.container__mision--text p{
-    width: 70%;
-    text-align: justify;
-    font-size: 25px;
+
+.container__mision--text h2 {
+  width: 100%;
+  text-align: center;
+  font-size: 40px;
 }
-.container__mision--img{
-    height: 500px;
-    width: 45vw;
-    background: linear-gradient(to bottom, #f600fe, #c900fe, #a136ff, #5142f0, #0033d9);
-    box-shadow: 4px 4px 2px 2px rgba(0,0,0,0.7);
-    border-radius: 6px;
-    display: flex;
-    justify-content: center;
-    align-items: center;
+
+.container__mision--text p {
+  width: 70%;
+  text-align: justify;
+  font-size: 25px;
 }
-.container__mision--img img{
-    height: 90%;
-    width: 90%;
-    object-fit: contain;
+
+.container__mision--img img {
+  height: 90%;
+  width: 90%;
+  object-fit: contain;
 }
+
 @media (max-width: 850px) {
-    .container__mision{
-        display: grid;
-        grid-template-columns: 1fr;
-    }
-    .container__mision--text{
-         flex-direction: column;
+  .container__mision {
+    display: grid;
+    grid-template-columns: 1fr;
     height: auto;
-    }
-    .container__mision--img{
-        width: 90vw;
-        height: auto;
-        padding-top: 10px;
-        padding-bottom: 10px;
-        margin: 20px auto;
-        grid-row:1;
-    }
-}
-@media (max-width: 850px) and (orientation: landscape){
-   
+  }
+
+  .container__mision--img {
+    width: 90vw;
+    height: auto;
+    padding-top: 10px;
+    padding-bottom: 10px;
+    margin: 20px auto;
+  }
 }
 </style>
-
- 
